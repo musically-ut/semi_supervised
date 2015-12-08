05994ca69d2ccc9741de74a6cd371cfba78ac40e
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-12-08 19:07:28 +0100

    Initial commit with a working version.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+from setuptools import setup
+
+setup(version="0.0.1",
+      name="semi_supervised",
+      author="Utkarsh Upadhyay",
+      license="MIT",
+      packages=["semi_supervised"],
+      setup_requires=["sklearn", "nose"],
+      )
